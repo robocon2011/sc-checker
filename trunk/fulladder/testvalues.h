48b7ac0b6c1f7971a0e51cb7d356139fbc5161b7
r.sollboeck@gmail.com <r.sollboeck@gmail.com@7935a1c5-0bca-9f7d-0f11-1cf652fbc1a5>
2011-10-01 20:07:46 +0000

    Project Fulladder added

---
+++ trunk/fulladder/testvalues.h
@@ -0,0 +1,28 @@
+#ifndef TESTVALUES_H
+#define TESTVALUES_H
+
+/*-------------------------------------
+ * Project: Fulladder
+ * ------------------------------------
+ * file: fulladder.cpp
+ * ------------------------------------
+ * 23.09.2o11: Sollböck, initial version
+ *
+ *
+ * ------------------------------------
+*/
+
+#include <systemc.h>
+#include <scv.h>
+
+struct testvalues
+{
+	enum type {SIMPLE, EXTENDED};
+	type mode;
+	sc_uint<8>	value_A;
+	sc_uint<8>	value_B;
+};
+
+
+
+#endif
