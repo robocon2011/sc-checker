e5b32e2f3376f1fcdfb87f6de5c110eb8fb635e8
r.sollboeck@gmail.com <r.sollboeck@gmail.com@7935a1c5-0bca-9f7d-0f11-1cf652fbc1a5>
2011-11-06 19:00:44 +0000

    Implementation of stimulator class
    ! working version !

--- work/stimulator/stimulator.cpp
+++ work/stimulator/stimulator.cpp
@@ -5,6 +5,34 @@
  *      Author: sollboer
  */
 
+#include <systemc>
+#include <scv.h>
+#include "stimulator.h"
+
+SC_HAS_PROCESS(stimulator_m);
+stimulator_m::stimulator_m(sc_module_name nm):
+		sc_module (nm)
+{
+
+	SC_METHOD (stimulator_method)
+			sensitive >> nextSample;
+}
+
+void stimulator::create_stimulator_module(sc_module_name name)
+{
+	i_stimulator(name);
+
+}
 
 
+int stimulator::create_testsequences(testsequence_c *testsequences)
+{
+	// IMPLEMENT_ME
+	return 0;
+}
 
+void stimulator_m::stimulator_method()
+{
+	// IMPLEMENT_ME
+
+}
--- work/stimulator/stimulator.h
+++ work/stimulator/stimulator.h
@@ -5,20 +5,63 @@
  *      Author: sollboer
  */
 
+/*
+ * Problems:
+ * 06.11.2011:
+ * - Modul innerhalb der Klasse stimulator kann bei Konstruktor nicht erzeugt werden,
+ * - sensitive Operator von SC_METHOD wird nicht erkannt
+ * - indirekter Konstruktor für stimulator_m durch create_stimulator_module hat noch Fehler
+ */
+
+
 #ifndef STIMULATOR_H_
 #define STIMULATOR_H_
 
 #include <systemc>
+using namespace sc_core;
 
-class stimulator {
+#include "scv.h"
+#include "stimulator_config.h"
+
+enum eConstraint {eCONSTR, eDIST};
+typedef eConstraint constraint_t;
+
+SC_MODULE (stimulator_m)
+{
+	// TODO: selbst definiertes Port für dutInput_t schreiben!
+	sc_inout < sc_uint<BITWIDTH> > input_A;
+	sc_inout < sc_uint<BITWIDTH> > input_B;
+	sc_in < bool > nextSample;
+
+	stimulator_m (sc_module_name nm);
+
+	void stimulator_method (void);
+};
+
+
+class testsequence_c {
 public:
-	int numOfTestsequences;
-	testsequence_t* testsequences;
+	testsequence_c () {}
 
-	int create_testsequences ( testsequence_t* testsequences);
-
+private:
+	constraint_t kindOfConstraint;
+	struct dutInput_t* lowerLimits;
+	struct dutInput_t* upperLimits;
 
 };
 
+
+class stimulator {
+public:
+	stimulator() {}
+	int create_testsequences ( testsequence_c* testsequences);
+	void create_stimulator_module (sc_module_name name);
+
+private:
+	int numOfTestsequences;
+	testsequence_c* testsequences;
+	stimulator_m i_stimulator;
+
+};
 
 #endif /* STIMULATOR_H_ */
