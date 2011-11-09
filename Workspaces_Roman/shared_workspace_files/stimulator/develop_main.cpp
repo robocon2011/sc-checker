928c5f46dc68e9c1135eefeb4d22af885cf66717
r.sollboeck@gmail.com <r.sollboeck@gmail.com@7935a1c5-0bca-9f7d-0f11-1cf652fbc1a5>
2011-11-09 17:29:33 +0000

    folders added to Workspace Roman

---
+++ Workspaces_Roman/shared_workspace_files/stimulator/develop_main.cpp
@@ -0,0 +1,24 @@
+/*
+ * develop_main.cpp
+ *
+ *  Created on: 06.11.2011
+ *      Author: ew08b033
+ */
+
+
+
+#include <systemc>
+#include <scv.h>
+#include "stimulator/stimulator.h"
+#include "stimulator/stimulator_config.h"
+#include "dut/fulladder/fulladder_rtl.h"
+
+int sc_main (int argc, char *argv[])
+{
+	fulladder_cascade i_fulladder("i_fulladder");
+	stimulator stimulator("i_stimulator");
+
+
+
+	return 0;
+}
