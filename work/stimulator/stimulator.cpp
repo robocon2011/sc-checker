eda89f3efbcfff7bd5256825fccd0aac7df3f87c
r.sollboeck@gmail.com <r.sollboeck@gmail.com@7935a1c5-0bca-9f7d-0f11-1cf652fbc1a5>
2011-11-14 05:24:09 +0000

    comments added to stimulator source files -- not finished --

--- work/stimulator/stimulator.cpp
+++ work/stimulator/stimulator.cpp
@@ -1,13 +1,33 @@
 /*
- * stimulator.cpp
+ * 	Filename:	stimulator.cpp
  *
- *  Created on: 02.11.2011
- *      Author: sollboer
+ *  Created on:	2011/11/13
+ *
+ *  Author: 	Roman SOLLBOECK
+ *
+ *  Project:	SystemC Checker
+ *  Submodule:	Stimulator
+ *
+ *  purpose:	generic function bodies for stimulator module
+ *
+ *  History:	2011(11/13: first executable version implemented
+ *
  */
 
 #include <systemc>
 #include <scv.h>
 #include "stimulator_config.h"
+
+/*
+ * **************************************************************
+ *
+ * functionname:	stimulator_m (constructor)
+ * purpose:			Constructor for sc_module stimulator_m
+ * parameters:		none
+ * returnvalue:		none
+ *
+ * **************************************************************
+ */
 
 SC_HAS_PROCESS(stimulator_m);
 stimulator_m::stimulator_m(sc_module_name nm)
@@ -17,47 +37,89 @@ stimulator_m::stimulator_m(sc_module_name nm)
 
 	SC_THREAD (stimulator_thread);
 }
+/*	stimulator_m	*/
 
+
+/*
+ * **************************************************************
+ *
+ * functionname:	create_testsequences
+ * purpose:			appends user-defined testsequences to stimulator
+ * 					by instantiation of user-specific class project_testsequences
+ * parameters:		none
+ * returnvalue:		none
+ *
+ * **************************************************************
+ */
 
 void stimulator_m::create_testsequences()
 {
+	/* instantiate user-specific class holding intended sequences */
 	project_testsequences pT(&testsequences);
 
+	/* extract total number of created sequences	*/
 	numOfTestsequences = testsequences.total_entries;
 
 }
+/*	create_testsequences	*/
 
+/*
+ * **************************************************************
+ *
+ * functionname:	stimulator_thread
+ * purpose:			SC_THREAD for generating stimulation outputs
+ * 					user-defined testsequences are proceeded in order of instantiation
+ * 					generator is blocked until new value is triggered by boolean control signal
+ * parameters:		none
+ * returnvalue:		none
+ *
+ * **************************************************************
+ */
 void stimulator_m::stimulator_thread()
 {
-	testseq_collectionentry_c *p_help;
-	unsigned int cnt_testcases;
+	testseq_collectionentry_c *p_help;	/*	helper pointer for switching between sequences	*/
+	unsigned int cnt_testcases;			/*	counter variable	*/
 
+	/*	init pointer with front instance of testsequences	*/
 	p_help = testsequences.p_priorEntry;
 
+	/*	crawl through doubly-linked testsequences until last element (first instance) is reached
+	 * (no pointer to prior element)	*/
 	while (p_help->p_priorEntry != 0)
 	{
 		p_help = p_help->p_priorEntry;
 	}
 
+	/*	loop through testsequences from first to last instance and generate
+	 * 	the specified amount of randomized values	*/
 	while (p_help != NULL)
 	{
 
 		for (cnt_testcases = 0; cnt_testcases < p_help->p_Sequence->no_of_testcases; cnt_testcases++)
 		{
+			/*	block loop until transition of boolean control signal	*/
 			wait(nextSample.value_changed_event());
 
+			/*	call SCV random generator function next() from currently loaded testsequence*/
 			p_help->p_Sequence->p_testvalues->next();
+			/*	wait statement for internal SystemC update-process	*/
 			wait(SC_ZERO_TIME);
+			/*	write generated values to module ports indirectly by user-defined callback function*/
 			write_values(p_help->p_Sequence->p_testvalues);
 		}
 
+		/*	load pointer with next testsequence instance	*/
 		p_help = p_help->p_lastEntry;
 	}
 
+	/*	indicate completion of all testsequences by boolean flag signal	*/
 	testsequences_finished.write(true);
 }
+/*	stimulator_thread	*/
 
 
 
-
+/*///////////////////////////////////////////////////////////////////////////////////////
+ * stimulator.cpp
+ */
 
--- work/stimulator/stimulator_config.h
+++ work/stimulator/stimulator_config.h
@@ -1,8 +1,18 @@
 /*
- * stimulator_config.h
+ * 	Filename:	stimulator_config.h
  *
- *  Created on: 04.11.2011
- *      Author: ew08b033
+ *  Created on:	2011/11/13
+ *
+ *  Author: 	Roman SOLLBOECK
+ *
+ *  Project:	SystemC Checker
+ *  Submodule:	Stimulator
+ *
+ *  purpose:	user-specific configuration for stimulator module
+ *  			config file for fulladder
+ *
+ *  History:	2011(11/13: first executable version implemented
+ *
  */
 
 #ifndef STIMULATOR_CONFIG_H_
@@ -12,18 +22,35 @@
 using namespace sc_core;
 using namespace sc_dt;
 #include <scv.h>
+/*	patch for dealing with issue from OSCI forum
+ * 	http://www.systemc.org/Discussion_Forums/systemc-forum/archive/msg?list_name=systemc-forum&monthdir=201005&msg=msg00010.html
+ */
+void _scv_pop_constraint();	/*	patch	*/
+
 #include "stimulator.h"
 
-
+/*	<ENTER BELOW>	------------------------------------------------
+ *
+ * 	define Bitwidth of fulladder design
+ *
+ */
 #define BITWIDTH 32
 
 
+/*	<ENTER BELOW>	------------------------------------------------
+ *
+ *	declare structure of user-specific source data
+ */
 struct dutInput_t
 {
 	sc_uint<BITWIDTH> input_A;
 	sc_uint<BITWIDTH> input_B;
 };
 
+/*	<ENTER BELOW>	------------------------------------------------
+ *
+ *	redefine user-specific source data for creating SCV-internal extensions interface
+ */
 SCV_EXTENSIONS(dutInput_t) {
 public:
 	scv_extensions < sc_uint<BITWIDTH> > input_A;
@@ -34,6 +61,9 @@ public:
 	}
 };
 
+/*	-> DO NOT MODIFY <-
+ * base constraint class for further constraint class definitions
+ */
 class dutInput_constraint_base_t
 	: public scv_constraint_base
 {
@@ -41,6 +71,14 @@ public:
 	scv_smart_ptr<dutInput_t> pInput;
 };
 
+/*	<ENTER BELOW>	------------------------------------------------
+ *
+ *	specialize base constraint class according SCV-guidelines
+ *	no limit for user-specific amount of specialized classes
+ *	Important:	use SCV_CONSTRAINT_CTOR,
+ *				specialize from dutInput_constraint_base_t,
+ *				redeclaration of scv_smart_ptr not necessary
+ */
 class dutInput_constraint_t_01
 	: public dutInput_constraint_base_t
 {
@@ -72,16 +110,33 @@ class dutInput_constraint_t_03
 	}
 };
 
+/*	<ENTER BELOW>	------------------------------------------------
+ *
+ *	declare class consisting of user-specific test sequences
+ *	testsequences are processed in the order of instantiation
+ */
 class project_testsequences
 {
 public:
+	/*	functionname:	project_testsequences (inline Constructor)
+	 *  purpose:		instantiation and registration of testsequences
+	 *  parameters:		p_testsequences...pointer to collection of all user-defined testsequences
+	 *  returnvalues:	none
+	 */
 	project_testsequences(testseq_collectionentry_c *p_testsequences)
 	{
-		addSequence(new testsequence_specialized_c < dutInput_constraint_t_01 > (p_testsequences, 3));
-		addSequence(new testsequence_specialized_c < dutInput_constraint_t_02 > (p_testsequences, 3));
-		addSequence(new testsequence_specialized_c < dutInput_constraint_t_03 > (p_testsequences, 5));
+		/*			|	templated class for testsequence 						|	pointer to collection		|	specific number of
+		 * 			|	user-defined constraint-classes							|	of testsequences (only one)	|	testcases (randoms)	*/
+		addSequence	(new testsequence_specialized_c < dutInput_constraint_t_01 > 	(p_testsequences, 				3));
+		/*			|															|								|						*/
+		addSequence	(new testsequence_specialized_c < dutInput_constraint_t_02 > 	(p_testsequences, 				3));
+		/*			|															|								|						*/
+		addSequence	(new testsequence_specialized_c < dutInput_constraint_t_03 > 	(p_testsequences, 				5));
+		/*			|															|								|						*/
+		/*	<ENTER> new testsequences	*/
 	}
 
+	/*	dummy function for catching return pointer of "new" operator	*/
 	void addSequence(void * dummy) {}
 };
 
